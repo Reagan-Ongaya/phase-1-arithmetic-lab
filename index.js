53fb6dbc0a233c49514a13623948718adc48cd57
reaganongaya <reagan.ongaya@student.moringaschool.com>
2022-11-30 12:19:47 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const multiply = (num1, num2) => {
+    return num1 * num2;
+};
+let resulMultiply = multiply(2, 31);
+console.log(resulMultiply);
 
-// Write your code here
+Math.floor(Math.random() >0);
+
+const mod = (num3, num4)=>{
+    return num3 / num4;
+};
+let resulDivision = mod(12, 3)
+console.log(resulDivision);
+
+const max = parseFloat(prompt("20: "))
+
+const largest = Math.max(20);
+
+console.log("The largest number is " + largest);
